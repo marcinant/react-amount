877631299ca92071b840d9e590892f44d6f4925d
Marcin Antczak <marcin.antczak@neutrico.eu>
2017-10-10 14:55:21 +0200

    fix homepage

--- src/AmountInput.jsx
+++ src/AmountInput.jsx
@@ -2,26 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function AmountInput(props) {
-  console.log('props', props.amount);
-  const {amount, lang, onChange} = props;
+  const {lang, onChange} = props;
   return (
     <input
       lang={lang}
       onChange={event => onChange({amount: event.target.value})}
       type="number"
-      // value={amount}
     />
   );
 }
 
 AmountInput.defaultProps = {
-  amount: 0,
   lang: 'pl',
   onChange: () => {},
 };
 
 AmountInput.propTypes = {
-  amount: PropTypes.number,
   lang: PropTypes.string,
   onChange: PropTypes.func,
 };
